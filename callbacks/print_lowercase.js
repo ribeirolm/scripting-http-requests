765db7f4b2416bb8bce9d393d52f8ccc7c997106
Lauren <laurenribeiro06@gmail.com>
2019-02-11 17:49:42 +0000

    Completed printLowerCase callback function

---
+++ callbacks/print_lowercase.js
@@ -0,0 +1,14 @@
+var https = require('https');
+var getHTML = require('../function.js');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path:  '/http-examples/step6/lowercase.html'
+};
+
+//Transforms the html string into ALL CAPS and prints it to the console.
+function printLowerCase (html) {
+  console.log(html.toLowerCase());
+}
+
+getHTML(requestOptions, printLowerCase);
